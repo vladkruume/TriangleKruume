a5d7c4a4861ab0de054ace825a4f07384ecee9f0
Opilane TTHK <opilane@tthk.ee>
2020-09-01 11:33:36 +0300

    Add project files.

---
+++ TriangleKruume/Triangle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleKruume
+{
+    class Triangle
+    {
+        public double a;
+        public double b;
+        public double c;
+        public Triangle(double A, double B, double C)
+        {
+            a = A;
+            b = B;
+            c = C;
+        }
+        public string outputA()
+        {
+            return Convert.ToString(a);
+        }
+        public string outputB()
+        {
+            return Convert.ToString(b);
+        }
+        public string outputC()
+        {
+            return Convert.ToString(c);
+        }
+        public double Perimeter()
+        {
+            double p = 0;
+            p = a + b + c;
+            return p;
+        }
+        public double Surface()
+        {
+            double s = 0;
+            double p = 0;
+            p = (a + b + c)/2;
+            s = Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+            return s;
+        }
+        public double GetSetA
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        public double GetSetB
+        {
+            get { return b; }
+            set { b = value; }
+        }
+        public double GetSetC
+        {
+            get { return c; }
+            set { c = value; }
+        }
+        public bool ExistTriange
+        {
+            get
+            {
+                if ((a > b + c) && (b > a + c) && (c > a + b))
+                    return false;
+                else return true;
+            }
+        }
+
+    }
+    
+
+}
+
