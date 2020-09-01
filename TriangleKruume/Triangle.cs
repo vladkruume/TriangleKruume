914a766c397d6bf368dd07fc7fd97c1e370b29ab
opilane <opilane@HP-CZC6297B91>
2020-09-01 12:13:11 +0300

    Добавил методы,функции связаные с высотой

--- TriangleKruume/Triangle.cs
+++ TriangleKruume/Triangle.cs
@@ -8,15 +8,27 @@ namespace TriangleKruume
 {
     class Triangle
     {
-        public double a;
+        public double a; 
         public double b;
         public double c;
+        public double h;
         public Triangle(double A, double B, double C)
         {
             a = A;
             b = B;
             c = C;
+
         }
+        public Triangle()
+        {
+        
+        }
+        public Triangle(double A,double H)
+        {
+            a = A;
+            h = H;
+        }
+
         public string outputA()
         {
             return Convert.ToString(a);
@@ -29,6 +41,11 @@ namespace TriangleKruume
         {
             return Convert.ToString(c);
         }
+        public string outputH()
+        {
+            return Convert.ToString(h);
+        }
+
         public double Perimeter()
         {
             double p = 0;
@@ -58,13 +75,18 @@ namespace TriangleKruume
             get { return c; }
             set { c = value; }
         }
+        public double GetSetH
+        {
+            get { return h; }
+            set { h = value; }
+        }
         public bool ExistTriangle
         {
             get
             {
-                if ((a > b + c) && (b > a + c) && (c > a + b))
-                    return false;
-                else return true;
+                if ((a < b + c) && (b < a + c) && (c < a + b))
+                    return true;
+                else return false;
             }
         }
 
